514304e959ba85078aee474e2d519a68fb99c9cf
randbw <apbward@gmail.com>
2015-05-30 15:38:06 +1000

    added controllers for all three models. added root_path, basic css and arguments#index. need to commit more often

---
+++ app/controllers/arguments_controller.rb
@@ -0,0 +1,23 @@
+class ArgumentsController < ApplicationController
+  def index
+    @arguments = Argument.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destory
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,40 @@
+# == Route Map
+#
+#                  Prefix Verb   URI Pattern                                                     Controller#Action
+#     user_argument_votes GET    /users/:user_id/arguments/:argument_id/votes(.:format)          votes#index
+#                         POST   /users/:user_id/arguments/:argument_id/votes(.:format)          votes#create
+#  new_user_argument_vote GET    /users/:user_id/arguments/:argument_id/votes/new(.:format)      votes#new
+# edit_user_argument_vote GET    /users/:user_id/arguments/:argument_id/votes/:id/edit(.:format) votes#edit
+#      user_argument_vote GET    /users/:user_id/arguments/:argument_id/votes/:id(.:format)      votes#show
+#                         PATCH  /users/:user_id/arguments/:argument_id/votes/:id(.:format)      votes#update
+#                         PUT    /users/:user_id/arguments/:argument_id/votes/:id(.:format)      votes#update
+#                         DELETE /users/:user_id/arguments/:argument_id/votes/:id(.:format)      votes#destroy
+#          user_arguments GET    /users/:user_id/arguments(.:format)                             arguments#index
+#                         POST   /users/:user_id/arguments(.:format)                             arguments#create
+#       new_user_argument GET    /users/:user_id/arguments/new(.:format)                         arguments#new
+#      edit_user_argument GET    /users/:user_id/arguments/:id/edit(.:format)                    arguments#edit
+#           user_argument GET    /users/:user_id/arguments/:id(.:format)                         arguments#show
+#                         PATCH  /users/:user_id/arguments/:id(.:format)                         arguments#update
+#                         PUT    /users/:user_id/arguments/:id(.:format)                         arguments#update
+#                         DELETE /users/:user_id/arguments/:id(.:format)                         arguments#destroy
+#                   users GET    /users(.:format)                                                users#index
+#                         POST   /users(.:format)                                                users#create
+#                new_user GET    /users/new(.:format)                                            users#new
+#               edit_user GET    /users/:id/edit(.:format)                                       users#edit
+#                    user GET    /users/:id(.:format)                                            users#show
+#                         PATCH  /users/:id(.:format)                                            users#update
+#                         PUT    /users/:id(.:format)                                            users#update
+#                         DELETE /users/:id(.:format)                                            users#destroy
+#
+
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root :to => 'arguments#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :users do
+   resources :arguments do
+    resources :votes
+    end
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
