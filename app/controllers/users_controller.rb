5cc73558b3e63f3b63bfbaf4370162c6d8f41ad8
randbw <apbward@gmail.com>
2015-06-02 18:29:42 +1000

    added flash notice so that once an account is created and it links to login page it displays name of account just created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
    @user = User.new user_params
     if @user.save
+      flash[:notice] = "Created account: #{@user.username}"
       redirect_to login_path
     else
       render :new
